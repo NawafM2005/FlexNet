10cb08621c33902eb566455a2c65904f7c430ebc
Nawaf <Nawaf.mahmood2005@gmail.com>
2025-05-13 14:57:34 -0400

    Updated...

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Dashboard.css"
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Profile from '../components/Profile';
 
 const Dashboard = () => {
+
+    const [currClicked, setCurrClicked] = useState("Dashboard");
+
     return (
-        <div>
+        <div className='dashboard_main'>
             <Navbar />
-            <h1>Dashboard</h1>
+            <div className='section_bar'>
+                <a onClick={() => setCurrClicked("Dashboard")} style={currClicked === "Dashboard" ? { backgroundColor: 'rgb(255, 0, 81)', pointerEvents: 'none'}: {}}>Dashboard</a>
+                <a onClick={() => setCurrClicked("Create")} style={currClicked === "Create" ? { backgroundColor: 'rgb(255, 0, 81)', pointerEvents: 'none'}: {}}>Create</a>
+                <a onClick={() => setCurrClicked("Profile")} style={currClicked === "Profile" ? { backgroundColor: 'rgb(255, 0, 81)', pointerEvents: 'none'}: {}}>Profile</a>
+            </div>
+
+            <Profile />
             <Footer />
         </div>
     );
